c392de33eff5d571f24aa15630741c8cd8838963
biancamarques17 <biancamthope@gmail.com>
2025-06-25 15:15:33 -0300

    href das paginas de cadastro

--- src/front/Ecofarma/Ecofarma-main/assets/js/Scripts/usuario.js
+++ src/front/Ecofarma/Ecofarma-main/assets/js/Scripts/usuario.js
@@ -154,13 +154,13 @@ function abrirCadastro(tipo) {
     // Aqui você pode redirecionar ou abrir o formulário de cadastro correspondente
     switch (tipo) {
         case "cliente":
-            window.location.href = "/src/front/Ecofarma/Ecofarma-main/cadastro-cliente.html";
+            window.location.href = "https://eco-farma.vercel.app/login-register.html/cadastro-cliente.html";
             break;
         case "farmacia":
-            window.location.href = "/src/front/Ecofarma/Ecofarma-main/cadastro-entregador.html";
+            window.location.href = "https://eco-farma.vercel.app/login-register.html/cadastro-entregador.html";
             break;
         case "entregador":
-            window.location.href = "/src/front/Ecofarma/Ecofarma-main/cadastro-farmacia.html";
+            window.location.href = "https://eco-farma.vercel.app/login-register.html/cadastro-farmacia.html";
             break;
         default:
             alert("Papel não reconhecido.");
